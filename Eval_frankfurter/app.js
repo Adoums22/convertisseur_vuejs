a71ba19d2be62cae96bc3940289ad94034a5481e
Adel Saadna <adel.saadna@soprasteria.com>
2024-01-14 13:19:00 +0100

    projet fini

--- Eval_frankfurter/app.js
+++ Eval_frankfurter/app.js
@@ -1,7 +1,7 @@
 Vue.createApp({
     data() {
         return {
-          amount: '',
+          amount: '1.00',
           from: '',
           to: '',
           error: '',
@@ -10,24 +10,36 @@ Vue.createApp({
         };
       },
       methods: {
+        swapCurrencies() {
+          const temp = this.from;
+          this.from = this.to;
+          this.to = temp;
+        },
         async fetchCurrencies() {
-            try {
+          try {
               const host = 'api.frankfurter.app';
               const response = await fetch(`https://${host}/currencies`);
               const data = await response.json();
               this.currencies = data;
-            } catch (error) {
+              return data; 
+          } catch (error) {
               const e = "Vous n'êtes pas connecté à internet";
               this.error = e;
               console.error(e);
-            }
-          },
+              throw error; 
+          }
+        },
         async search() {
           if (!this.amount || isNaN(this.amount)) {
             this.error = "Veuillez entrer un montant valide";
             return;
           }
-    
+          
+          if (this.amount <= 0) {
+            this.error = "Veuillez saisir un montant supérieur à 0";
+            return;
+          }
+
           this.error = "";
     
           try {
@@ -42,8 +54,14 @@ Vue.createApp({
           }
         },
       },
-      mounted() {
-        this.fetchCurrencies(); // Pour récupérer la liste des devises au chargement de la page
-      }
+      async mounted() {
+        try {
+            const fetchedCurrencies = await this.fetchCurrencies();
+            this.from = Object.keys(fetchedCurrencies)[0];
+            this.to = Object.keys(fetchedCurrencies)[1];
+        } catch (error) {
+            console.error("Erreur lors du chargement des devises", error);
+        }
+    }
   }).mount("#app");
   
